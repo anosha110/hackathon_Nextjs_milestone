a7f8610b95ac2fd10f648981d04c1578dd097e43
Anosha Mohsin <anoshasyed18@gmail.com>
2024-11-10 12:33:45 +0500

    Added project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-import Navbar from "@/components/Navbar";
+
 import Hero from "@/components/Hero";
-import Image from "next/image";
-import Footer from "@/components/Footer";
+
 
 export default function Home() {
   return (
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,10 +7,10 @@ const Hero = () => {
      
       <div className="flex-1 md:pr-8 mb-6 md:mb-0 text-center md:text-left">
         <h2 className="text-xl font-bold mb-2 text-gray-400 mx-12" >HELLO</h2>
-        <h3 className="text-3xl font-semibold mb-2 mx-12">I'm Anosha</h3>
+        <h3 className="text-3xl font-semibold mb-2 mx-12">I am Anosha</h3>
         <p className="text-xl mb-4 mx-12">A Web Developer</p>
         <p className="text-base px-2 md:px-0 mx-12">
-          Welcome to my web developer portfolio! I'm Anosha, a skilled and creative web developer with a passion for creating beautiful, responsive, and user-friendly websites. I've worked on a variety of web projects, ranging from personal blogs to e-commerce platforms.
+          Welcome to my web developer portfolio! Im Anosha, a skilled and creative web developer with a passion for creating beautiful, responsive, and user-friendly websites. I have worked on a variety of web projects, ranging from personal blogs to e-commerce platforms.
         </p>
         <div className="flex justify-center md:justify-start  my-8">
           <button className="inline-flex mx-12 items-center border-2 border-custom-purple bg-custom-purple text-white rounded text-base py-2 px-6 focus:outline-none hover:bg-purple-700 hover:border-purple-700">
